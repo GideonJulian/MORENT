0ed68dacc1e12bece10e6134d65951e3ba110626
Gideon <125488605+GideonJulian@users.noreply.github.com>
2025-02-10 13:07:23 +0100

    category page added

--- src/CategoryLayout/CategoryLayout.jsx
+++ src/CategoryLayout/CategoryLayout.jsx
@@ -2,126 +2,83 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
+import { useState } from "react";
 import Footer from "../components/Footer";
 const CategoryLayout = () => {
+  const [isopen, setIsOpen] = useState(false);
+  const openSidebar = () => {
+    setIsOpen(true);
+    console.log('clicked ')
+  };
+const CloseSidebar  = () => {
+  setIsOpen(false);
+ }
   return (
     <div>
-      <header className="w-full p-6 bg-white flex justify-between item-center fixed z-10">
-        <div className="flex gap-20 flex-col md:flex-row">
-          <div>
-            <h3 className="font-bold text-2xl text-primary-500"> MORENT</h3>
-          </div>
-          <div className="relative ml-3 hidden md:block">
-            <i class="bi bi-search  absolute top-2 left-4"></i>
-            <input
-              type="text"
-              className="pl-10 h-10 px-4 py-2 rounded-full w-[200px] md:w-[450px] border "
-              placeholder="Search something here "
-            />
-            <i class="bi bi-sliders absolute top-2 right-5"></i>
-          </div>
-        </div>
-        <div className="flex items-center gap-3 ">
-          <div className="hidden items-center justify-center border rounded-full h-10 w-10  md:flex">
-            <i className="bi bi-heart-fill text-gray-400"></i>
-          </div>
-          <div className="hidden items-center justify-center border rounded-full h-10 w-10  md:flex">
-            <i class="bi bi-bell-fill text-gray-400"></i>
-          </div>
-          <div className=" items-center justify-center border rounded-full h-10 w-10 hidden md:flex">
-            <i class="bi bi-gear-fill text-gray-400"></i>
-          </div>
+      <header className="w-full p-6 bg-white  fixed z-10 top-0">
+        <div className="flex justify-between items-center  md:hidden ">
+          <i
+            className="bi bi-list text-4xl block md:hidden cursor-pointer"
+            onClick={openSidebar}
+          ></i>
+          ;
           <div className="flex items-center justify-center border rounded-full h-10 w-10">
             <i class="bi bi-person-fill text-gray-400"></i>
           </div>
         </div>
+        <div className="container flex justify-between items-center ml-2">
+          <div className="flex gap-10 flex-col md:flex-row">
+            <div className="mt-5 md:mt-0">
+              <h3 className="font-bold text-2xl text-primary-500"> MORENT</h3>
+            </div>
+            <div className="block md:hidden relative">
+              <i class="bi bi-search absolute top-3 left-5"></i>
+
+              <div className="flex items-center justify-between">
+                <input
+                  type="text"
+                  name=""
+                  id=""
+                  placeholder="Search something here"
+                  className="px-4 py-3  rounded-md border w-full pl-10 pr-10"
+                />
+                <div className="px-2 py-1 ml-5 border rounded-md text-center">
+                <i class="bi bi-sliders text-3xl "></i>
+                </div>
+              </div>
+            </div>
+            <div className="relative ml-3 hidden md:block">
+              <i class="bi bi-search  absolute top-2 left-4"></i>
+              <input
+                type="text"
+                className="pl-10 h-10 px-4 py-2 rounded-full w-[200px] md:w-[450px] border "
+                placeholder="Search something here "
+              />
+              <i class="bi bi-sliders absolute top-2 right-5"></i>
+            </div>
+          </div>
+          <div className="flex items-center gap-3 ">
+            <div className="hidden items-center justify-center border rounded-full h-10 w-10  md:flex">
+              <i className="bi bi-heart-fill text-gray-400"></i>
+            </div>
+            <div className="hidden items-center justify-center border rounded-full h-10 w-10  md:flex">
+              <i class="bi bi-bell-fill text-gray-400"></i>
+            </div>
+            <div className=" items-center justify-center border rounded-full h-10 w-10 hidden md:flex">
+              <i class="bi bi-gear-fill text-gray-400"></i>
+            </div>
+            <div className=" items-center justify-center border hidden md:flex rounded-full h-10 w-10">
+              <i class="bi bi-person-fill text-gray-400"></i>
+            </div>
+          </div>
+        </div>
       </header>
       <div className="flex">
-        <Sidebar />
-        <div className="p-4 ml-[19rem] mt-20 w-full">
+        <Sidebar IsOpen={isopen} onClose={CloseSidebar}/>
+        <div className="p-4 ml-1 md:ml-[19rem] mt-60 md:mt-20 w-full">
           <Outlet />
         </div>
       </div>
-      {/* <footer>
-        <div className="w-full  bg-white ">
-          <div className="container flex flex-col md:flex-row md:mb-8 mb-0  px-10 py-10 justify-between  mx-auto">
-            <div>
-              <div>
-                <h3 className="font-bold text-2xl text-primary-500"> MORENT</h3>
-              </div>
-              <p className="w-[300px] md:text-black text-secondary-500 font-sans">
-                Our vision is to provide convenience and help increase your
-                sales business.{" "}
-              </p>
-            </div>
-            <div className="flex gap-16 md:mt-0 mt-10">
-              <div>
-                <h3 className="text-lg font-bold font-sans">Community</h3>
-                <ul className="flex flex-col md:gap-7 gap-4  md:mt-10 mt-5">
-                  <li>
-                    <Link className="md:text-black text-secondary-500 font-sans">
-                      Events{" "}
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="md:text-black text-secondary-500 font-sans">
-                      Blog{" "}
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="md:text-black text-secondary-500 font-sans">
-                      Podcast{" "}
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="md:text-black text-secondary-500 font-sans">
-                      Invite a friend{" "}
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <h3 className="text-lg font-bold font-sans">About</h3>
-                <ul className="flex flex-col md:gap-7 gap-4 md:mt-10 mt-5">
-                  <li>
-                    <Link className="md:text-black text-secondary-500 font-sans">
-                      How it works{" "}
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="md:text-black text-secondary-500 font-sans">
-                      Featured{" "}
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="md:text-black text-secondary-500 font-sans">
-                      Partnership{" "}
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="md:text-black text-secondary-500 font-sans">
-                      Bussiness Relation{" "}
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div className="bottom pt-5 md:border-t-2 border-t-0 p-10 ">
-            <div className="flex justify-normal md:justify-between flex-col-reverse md:flex-row md:items-center items-baseline">
-              <div className="">
-                <h3 className="font-bold font-sans">
-                  ©2022 MORENT. All rights reserved
-                </h3>
-              </div>
-              <div className="flex gap-10 md:justify-between md:mb-0 mb-10">
-                <h3 className="font-bold font-sans">Privacy & Policy</h3>
-                <h3 className="font-bold font-sans">Terms & Condition</h3>
-              </div>
-            </div>
-          </div>
-        </div>
-      </footer> */}
     </div>
   );
 };
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,37 +1,91 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const Sidebar = () => {
+const Sidebar = ({ IsOpen, onClose }) => {
+  const [checkedState, setCheckedState] = useState({
+    sports: false,
+    suv: false,
+    mpv: false,
+    sedan: false, // Fix typo from 'sodan' to 'sedan'
+    coupe: false,
+    hatchback: false,
+  });
+
+  const types = [
+    { label: "Sports", key: "sports", count: 10 },
+    { label: "SUV", key: "suv", count: 12 },
+    { label: "MPV", key: "mpv", count: 16 },
+    { label: "Sedan", key: "sedan", count: 20 },
+    { label: "Coupe", key: "coupe", count: 14 },
+    { label: "Hatchback", key: "hatchback", count: 14 },
+  ];
+  const capacity = [
+    { label: "2", key: "2", count: 10 },
+    { label: "4", key: "4", count: 12 },
+    { label: "6", key: "5", count: 16 },
+
+    { label: "8", key: "8", count: 14 },
+  ];
+
+  const handleCheckBoxChange = (type) => {
+    setCheckedState((prevState) => ({
+      ...prevState,
+      [type]: !prevState[type],
+    }));
+  };
+
   return (
-    <div className='fixed -z-10 h-screen w-[300px] p-4 bg-white border-red-500'>
-     <div className="mt-20">
-     <h1>Sidebar </h1>
-     <h1>Sidebar </h1>
-     <h1>Sidebar </h1>
-     <h1>Sidebar </h1>
-     <h1>Sidebar </h1>
-     <h1>Sidebar </h1>
-     <h1>Sidebar </h1>
-     <h1>Sidebar </h1>
-     <h1>Sidebar </h1>
-     <h1>Sidebar </h1>
-     <h1>Sidebar </h1>
-     <h1>Sidebar </h1>
-     <h1>Sidebar </h1>
-     <h1>Sidebar </h1>
-     <h1>Sidebar </h1>
-     <h1>Sidebar </h1>
-     <h1>Sidebar </h1>
-     <h1>Sidebar </h1>
-     <h1>Sidebar </h1>
-     <h1>Sidebar </h1>
-     <h1>Sidebar </h1>
-     <h1>Sidebar </h1>
-     <h1>Sidebar </h1>
-     <h1>Sidebar </h1>
-     <h1>Sidebar </h1>
-     </div>
+    <div
+      className={`fixed z-10 h-screen transition-all duration-300 shadow-md ${
+        IsOpen ? "w-[280px] p-4" : "w-0 p-0"
+      }  md:w-[280px]  md:p-4  bg-white border-red-500 mt-[5%]`}
+    >
+      <div className="md:hidden cursor-pointer">
+        <i class="bi bi-x-lg font-[400] text-lg float-right" onClick={onClose} ></i>
+      </div>
+      <div className="mt-5 py-4 mb-10 overflow-y-auto h-[calc(100%-5rem)]">
+        <div className="type">
+          <p className="text-lg text-gray-500 font-sans">Type</p>
+          {types.map((item) => (
+            <div className="flex items-center mt-5" key={item.key}>
+              <input
+                type="checkbox"
+                name="type"
+                className="mr-2 text-lg"
+                checked={checkedState[item.key]}
+                onChange={() => handleCheckBoxChange(item.key)}
+              />
+              <label className="font-semibold font-sans">
+                {item.label}{" "}
+                <span className="font-normal text-gray-500">
+                  ({item.count})
+                </span>
+              </label>
+            </div>
+          ))}
+        </div>
+        <div className="type mt-10 mb-5">
+          <p className="text-lg text-gray-500 font-sans">Capacity</p>
+          {capacity.map((item) => (
+            <div className="flex items-center mt-5" key={item.key}>
+              <input
+                type="checkbox"
+                name="type"
+                className="mr-2 text-lg"
+                checked={checkedState[item.key]}
+                onChange={() => handleCheckBoxChange(item.key)}
+              />
+              <label className="font-semibold font-sans">
+                {item.label}{" "}
+                <span className="font-normal text-gray-500">
+                  ({item.count})
+                </span>
+              </label>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
