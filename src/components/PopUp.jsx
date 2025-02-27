f00324178ad188eb2f848be3ac009f46c5bc855a
Gideon Julian <gideonchinonso77@gmail.com>
2025-02-28 00:29:16 +0100

    Modal fixed

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -2,10 +2,13 @@ import React from "react";
 
 const PopUp = () => {
   return (
-    <div className="fixed top-20 left-1/2 transform -translate-x-1/2 bg-white px-10 py-4 rounded-lg shadow-lg text-lg font-semibold flex items-center gap-2 transition-all duration-500 ease-in-out animate-slideDown">
+    <div className="w-full flex justify-center">
+    <div className="fixed top-20 left-1/2 transform -translate-x-1/2 z-10 bg-white px-10 py-4 rounded-lg shadow-lg text-lg font-semibold flex items-center gap-2 transition-all whitespace-nowrap min-w-max duration-500 ease-in-out animate-slideDown">
       <i className="bi bi-check-circle-fill text-green-500"></i>
       Added to favourite
     </div>
+  </div>
+  
   );
 };
 
