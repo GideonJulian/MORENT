8a273f9532cdd6f07fa5ba6689d0c8539d454e03
Gideon Julian <gideonchinonso77@gmail.com>
2025-03-01 06:20:25 +0100

    favourite card fixed

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -26,7 +26,10 @@ const Favourites = ({ onclose }) => {
           </h1>
           {favouriteCars.map((car) => (
             <div>
-              <div key={car.id} className="md:p-5 p-5 rounded-lg shadow-lg mb-5 w-auto md:w-auto border">
+              <div
+                key={car.id}
+                className="md:p-5 p-5 rounded-lg shadow-lg mb-5 w-auto md:w-auto border"
+              >
                 <div className="flex items-center gap-3 justify-between md:flex-row flex-col">
                   <img
                     src={car.img}
@@ -34,15 +37,21 @@ const Favourites = ({ onclose }) => {
                     className="h-20 w-[230px]"
                   />
 
-                  <div className="md:block flex  items-center justify-center gap-2">
-                    <h3 className="font-bold md:text-lg text-sm ">{car.carName}</h3>
-                    <p className="text-gray-500 text-lg">{car.category}</p>
-                    <h3 className="mt-3 font-bold md:text-lg text-sm">${car.price}/day</h3>
+                  <div className="md:block flex md:flex-row flex-col gap-0 md:items-center  md:justify-center md:gap-2">
+                    <div className="flex items-center gap-2">
+                      <h3 className="font-bold md:text-lg text-sm ">
+                        {car.carName}
+                      </h3>
+                      <p className="text-gray-500 text-lg">{car.category}</p>
+                    </div>
+                    <h3 className="md:mt-3 font-bold md:text-lg text-sm">
+                      ${car.price}/day
+                    </h3>
                   </div>
                 </div>
                 <div className="mt-3 flex gap-2 justify-end">
                   <button className="px-4 py-2 rounded-lg text-white  bg-green-500">
-                  <span className="md:block hidden">Add to cart</span>
+                    <span className="md:block hidden">Add to cart</span>
                     <i class="bi bi-cart-plus md:hidden block"></i>
                   </button>
                   <button className="md:px-4 px-2  py-1 rounded-lg text-white  bg-red-500">
